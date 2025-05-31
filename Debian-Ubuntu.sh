ed8df0d51f027c3605279caf98b062177951726f
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-05-31 01:22:59 -0700

    Update Debian-Ubuntu.sh

--- Debian-Ubuntu.sh
+++ Debian-Ubuntu.sh
@@ -14,16 +14,13 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] http
 curl -fsSL https://ppa.floorp.app/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.floorp.app/Floorp.list'
 
-# Update Package Lists
-# sudo apt update
-
 # Adds 32 bit packages, Update Package Lists, and install WINE + 32-bit Libraries (Mainly for Lutris).
+# Code taken from Lutris Documentation
 sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y wine64 wine32 libasound2-plugins:i386 libsdl2-2.0-0:i386 libdbus-1-3:i386 libsqlite3-0:i386
 
 # Install Apps/Libraries
 # Installing GameScope this way only works on Debian for now,
-# however it shouldn't hurt on an Ubuntu-Based system, it just may not install.
-# GameScope may not work with Nvidia GPUs anyway
+# however it shouldn't hurt on an Ubuntu-Based system, it just may not install. GameScope may not work with Nvidia GPUs anyway
 sudo apt install -y lutris
 sudo apt install -y brave-browser
 sudo apt install -y floorp
@@ -38,7 +35,7 @@ sudo apt install -y htop
 sudo apt install -y gparted
 sudo apt install -y git
 
-# Update all packages
+# Upgrade all installed packages
 sudo apt upgrade
 
 # Clean Package Cache
