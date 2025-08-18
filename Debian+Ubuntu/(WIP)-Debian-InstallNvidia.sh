b1ae1a020c61d8174b36bbd2aaa2015bda9c29cc
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-08-17 19:15:00 -0700

    Create (WIP)-Debian-InstallNvidia.sh

---
+++ Debian+Ubuntu/(WIP)-Debian-InstallNvidia.sh
@@ -0,0 +1,5 @@
+# from
+# Debian Bookworm
+sudo deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+sudo apt update
+sudo apt install nvidia-driver firmware-misc-nonfree
