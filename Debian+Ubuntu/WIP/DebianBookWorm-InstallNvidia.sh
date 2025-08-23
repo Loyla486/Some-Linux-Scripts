9db4f503b196d3325fe701011bd1c15e645fcee5
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-08-23 14:49:51 -0700

    Update DebianBookWorm-InstallNvidia.sh

--- Debian+Ubuntu/WIP/DebianBookWorm-InstallNvidia.sh
+++ Debian+Ubuntu/WIP/DebianBookWorm-InstallNvidia.sh
@@ -2,6 +2,7 @@
 # from https://wiki.debian.org/NvidiaGraphicsDrivers#Prerequisites
 # apt install nvidia-open-kernel-dkms nvidia-driver firmware-misc-nonfree
 # Debian Bookworm
-sudo deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
-sudo apt update
-sudo apt install nvidia-driver firmware-misc-nonfree
+# sudo deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+# sudo apt update
+# sudo apt install nvidia-driver firmware-misc-nonfree
+exit
