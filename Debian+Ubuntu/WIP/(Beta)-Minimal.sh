6c216dd6e6205be3b0184b06ae59ff9c2c87a8b7
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-08-23 14:44:18 -0700

    Update (Beta)-Minimal.sh

--- Debian+Ubuntu/WIP/(Beta)-Minimal.sh
+++ Debian+Ubuntu/WIP/(Beta)-Minimal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Intended for Debian/Unbuntu Based Distros and netbooks with low storage (32GB-64GB/28GiB-58GiB)
-# This is also intended for netbooks, chromebooks, and low end PCs..
+# This is also intended for netbooks, chromebooks, and/or low end PCs..
 # Version 3 Beta
 
 # Ensure curl is installed
