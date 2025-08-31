21fb127d8041b0b9560583972c244f72ca6abea1
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-08-31 11:14:21 -0700

    Update XFCE-Install-Bluetooth.sh

--- Debian+Ubuntu/XFCE-Install-Bluetooth.sh
+++ Debian+Ubuntu/XFCE-Install-Bluetooth.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # From: https://www.reddit.com/r/xfce/comments/7m36o9/comment/drqzxar/
 # Install bluetooth on default/plain XFCE installs (Debian etc.)
 sudo apt install bluetooth pulseaudio-module-bluetooth blueman bluez-firmware -y
