ca3408e3d64f5fbbb91fb826abd25ec575099dd4
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-09-11 12:48:06 -0700

    Create Install-Broadcom-Driver2.sh

---
+++ Debian+Ubuntu/WIP/Dell E6400/Install-Broadcom-Driver2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# from https://askubuntu.com/questions/612255/wireless-not-working-on-dell-latitude-e6400-with-ubuntu-14-04-2
+# Make sure you are connected to ethernet first.
+sudo apt update
+sudo apt remove firmware-b43-installer && sudo apt install --reinstall bcmwl-kernel-source
