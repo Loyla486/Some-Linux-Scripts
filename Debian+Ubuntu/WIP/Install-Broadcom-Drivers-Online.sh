86fbb4f44d5c6a564d1b5adc9b66994f77edaf47
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-09-11 12:07:51 -0700

    Update and rename Install-Broadcom-Drivers.sh to Install-Broadcom-Drivers-Online.sh

--- Debian+Ubuntu/WIP/Install-Broadcom-Drivers-Online.sh
+++ Debian+Ubuntu/WIP/Install-Broadcom-Drivers-Online.sh
@@ -3,8 +3,3 @@
 # Make sure you are connected to ethernet first.
 $ sudo apt-get update
 $ sudo apt-get install firmware-b43-installer
-# or
-$ cd ~
-$ sudo dpkg -i b43-fwcutter*
-$ tar xfvj broadcom-wl-5.100.138.tar.bz2
-$ sudo b43-fwcutter -w /lib/firmware broadcom-wl-5.100.138/linux/wl_apsta.o
