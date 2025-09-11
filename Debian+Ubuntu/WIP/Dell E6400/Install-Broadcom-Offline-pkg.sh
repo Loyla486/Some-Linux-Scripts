882b05c9ba21586174c052c22917337a257b8957
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-09-11 12:09:21 -0700

    Create Install-Broadcom-Offline-pkg.sh

---
+++ Debian+Ubuntu/WIP/Dell E6400/Install-Broadcom-Offline-pkg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# from https://askubuntu.com/questions/612255/wireless-not-working-on-dell-latitude-e6400-with-ubuntu-14-04-2
+$ cd ~
+$ sudo dpkg -i b43-fwcutter*
+$ tar xfvj broadcom-wl-5.100.138.tar.bz2
+$ sudo b43-fwcutter -w /lib/firmware broadcom-wl-5.100.138/linux/wl_apsta.o
