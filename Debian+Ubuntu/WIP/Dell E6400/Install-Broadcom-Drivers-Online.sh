bcf387b78b71145342beff58d65cf5902a3bccf7
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-09-11 12:32:02 -0700

    Update Install-Broadcom-Drivers-Online.sh

--- Debian+Ubuntu/WIP/Dell E6400/Install-Broadcom-Drivers-Online.sh
+++ Debian+Ubuntu/WIP/Dell E6400/Install-Broadcom-Drivers-Online.sh
@@ -4,4 +4,4 @@
 sudo apt-get update
 sudo apt-get install firmware-b43-installer
 # from https://forums.linuxmint.com/viewtopic.php?p=1866927&sid=b393b85a19e94f8b24235536ec4c31c4#p1866927
-sudo apt remove firmware-b43-installer && sudo apt install --reinstall bcmwl-kernel-source 
+sudo apt remove firmware-b43-installer && sudo apt install --reinstall bcmwl-kernel-source
