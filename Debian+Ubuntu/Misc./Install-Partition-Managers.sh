583e3db036f86cbbe1052586d810953ce889ee88
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-09-20 14:38:02 -0700

    Create Install-Partition-Managers.sh

---
+++ Debian+Ubuntu/Misc./Install-Partition-Managers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install -y partitionmanager
+sudo apt install -y kde-partitionmanager
+sudo apt install -y gnome-disk-utility
+sudo apt install -y gparted
+sudo apt install -y exfatprogs
