bba5b15d4fa4d2a756452b27f378e69a1c309a16
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-10-07 12:38:57 -0700

    Update Remove-MozillaStuff+Locales.sh
    Cleaned up comment

--- Debian+Ubuntu/Remove-MozillaStuff+Locales.sh
+++ Debian+Ubuntu/Remove-MozillaStuff+Locales.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script removes Firefox and Thunderbird and the locales.
+# This script removes Firefox, Thunderbird, and the locales.
 # MAKE SURE YOU ONLY RUN THIS IF YOU HAVE ANOTHER WEB BROWSER INSTALLED!
 sudo apt remove firefox* -y
 sudo apt remove thunderbird* -y
