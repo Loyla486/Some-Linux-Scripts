2ec16c78a4ada08f6fb2075825a4d5c66f1fc611
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-10-07 12:45:15 -0700

    Update Debian+Ubuntu-Minimal-Netbook.sh
    added "# This will remove Firefox and Thunderbird!"

--- Debian+Ubuntu/Debian+Ubuntu-Minimal-Netbook.sh
+++ Debian+Ubuntu/Debian+Ubuntu-Minimal-Netbook.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Intended for Debian/Unbuntu Based Distros and netbooks with low storage (32GB-64GB/28GiB-58GiB)
 # This is also intended for netbooks, chromebooks, and/or low end PCs..
+# This will remove Firefox and Thunderbird!
 # Version 4 Beta
 
 # Start with hardinfo since newer versions requires attendance.
