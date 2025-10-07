b4604ba3cad4ac61f4926d8da1260b256d89f82b
Loyla <159725294+Loyla486@users.noreply.github.com>
2025-10-07 12:35:11 -0700

    Update Debian+Ubuntu-Minimal-Netbook.sh
    added "# Floorp's PPA (Repo) is stuck on 11.30.0, so it is being commented out for now."

--- Debian+Ubuntu/Debian+Ubuntu-Minimal-Netbook.sh
+++ Debian+Ubuntu/Debian+Ubuntu-Minimal-Netbook.sh
@@ -13,6 +13,7 @@ sudo apt install -y curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
+# Floorp's PPA (Repo) is stuck on 11.30.0, so it is being commented out for now.
 # Add Floorp Repository (Code taken from Floorp Website) (https://ppa.floorp.app/)
 # curl -fsSL https://ppa.floorp.app/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
 # sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.floorp.app/Floorp.list'
