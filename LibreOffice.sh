9fbf1196834e576fb04977de65e59ecb6ef13fa7
Loyla - i486 <159725294+Loyla486@users.noreply.github.com>
2026-01-03 23:57:03 -0800

    Create LibreOffice.sh

---
+++ LibreOffice.sh
@@ -0,0 +1,2 @@
+libreoffice --norestore --show "/home/loyla/Desktop/Untitled 1.odp"
+# Command taken and modified from: https://stackoverflow.com/questions/67196386/start-libreoffice-in-presentation-mode-on-boot-raspberry-pi
