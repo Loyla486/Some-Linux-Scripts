4b519287bdd747a205c809797c942c677480dafb
Loyla - i486 <159725294+Loyla486@users.noreply.github.com>
2026-01-11 23:58:48 -0800

    Update LibreOffice.sh

--- LibreOffice.sh
+++ LibreOffice.sh
@@ -1,2 +1,3 @@
-libreoffice --norestore --show "/home/loyla/Desktop/Untitled 1.odp"
 # Command taken and modified from: https://stackoverflow.com/questions/67196386/start-libreoffice-in-presentation-mode-on-boot-raspberry-pi
+# Open LibreOffice and the specified file, and exit LibreOffice when you leave the presentation.
+libreoffice --norestore --show "/home/loyla/Desktop/Untitled 1.odp"
